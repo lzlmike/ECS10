db6e89d773aa46864abfdaca6a9b13c6ca6427e4
Zuolin Li  (Mike) <lzlli@ucdavis.edu>
2015-10-19 22:18:05 -0500

    Create capital.py

---
+++ HW1/capital.py
@@ -0,0 +1,11 @@
+def main():
+    Str=str(input("Please enter a string: "))
+    Str=Str.lower()
+    Str=Str.replace("a","A")
+    Str=Str.replace("e","E")
+    Str=Str.replace("i","I")
+    Str=Str.replace("o","O")
+    Str=Str.replace("u","U")
+    print(Str)
+
+main()
