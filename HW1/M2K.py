31fcc63679cb1e6e12e19c7cb6ae5201fd5967ea
Zuolin Li  (Mike) <lzlli@ucdavis.edu>
2015-10-19 22:19:41 -0500

    Create M2K.py

---
+++ HW1/M2K.py
@@ -0,0 +1,7 @@
+def main():
+    Mile=eval(input("Please enter the distance in miles: "))
+    To_Kilometer=1.60934*Mile
+    print("%.2f miles is %.2f kilometers"%(Mile,To_Kilometer))
+
+main()
+    
